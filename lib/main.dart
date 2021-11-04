a73afdaba974da6fcc2249f17403850ad1cb4441
rafacardososales <rafael.moaz@yahoo.com.br>
2021-11-04 00:35:40 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:radio_app/pages/home_page.dart';
+
+
+void main(){
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Radio App",
+      theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
